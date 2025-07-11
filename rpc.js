9e5bd97abb278c7dfa71c175e37992ad1ece9ded
HasstDivi <jrmal2021@gmail.com>
2025-07-11 18:13:44 +0200

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,7 +1,26 @@
-const rpcConfig = {
-  host: 'rpc.divi.foundation',
-  port: 51473,
-  user: 'rpcuser',
-  pass: 'rpcpass'
-};
+const RPC_USER = "diviuser";
+const RPC_PASSWORD = "divipass123";
+const RPC_PORT = 51473;
+const RPC_HOST = "http://TU_IP_PUBLICA_DEL_NODO"; // Cambia esto por tu IP
+
+async function rpcCall(method, params = []) {
+  const body = {
+    jsonrpc: "1.0",
+    id: "curltext",
+    method,
+    params,
+  };
+
+  const response = await fetch(`${RPC_HOST}:${RPC_PORT}/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + btoa(`${RPC_USER}:${RPC_PASSWORD}`),
+    },
+    body: JSON.stringify(body),
+  });
+
+  const data = await response.json();
+  return data.result;
+}
 
