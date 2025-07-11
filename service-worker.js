cc2400383d432656edc20f505cb3eabfe3c68d5c
HasstDivi <jrmal2021@gmail.com>
2025-07-11 15:16:51 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'divimonster-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './wallet.js',
+  './staking.js',
+  './rpc.js',
+  './manifest.json',
+  './divimonster.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
