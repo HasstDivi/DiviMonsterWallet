7833b26e2a16a58dbcfbd792ff0c9b864f7e7b1d
HasstDivi <jrmal2021@gmail.com>
2025-07-12 21:56:00 +0200

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,29 +1,58 @@
 // rpc.js
-const RPC_USER = "DivinoMonstruo";
-const RPC_PASSWORD = "divipachuli123";
-const RPC_PORT = 51473;
-const RPC_HOST = "138.68.94.212";
 
-async function rpcCall(method, params = []) {
-  const response = await fetch(`http://${RPC_HOST}:${RPC_PORT}/`, {
-    method: "POST",
+const NODE_URL = 'http://[138.68.94.212]:51473'; 
+const RPC_USER = 'divimonster';
+const RPC_PASSWORD = 'divipachuli123';
+
+async function callRPC(method, params = []) {
+  const response = await fetch(NODE_URL, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa(`${RPC_USER}:${RPC_PASSWORD}`)
     },
     body: JSON.stringify({
-      jsonrpc: "1.0",
-      id: "DiviMonsterWallet",
+      jsonrpc: '1.0',
+      id: 'diviwallet',
       method,
-      params,
-    }),
+      params
+    })
   });
 
-  const data = await response.json();
-  if (data.error) {
-    throw new Error(data.error.message);
-  }
-  return data.result;
+  const json = await response.json();
+  if (json.error) throw new Error(json.error.message);
+  return json.result;
 }
+
+// Funciones específicas
+export async function getBalance(address) {
+  return await callRPC('getaddressbalance', [address]);
+}
+
+export async function createVault(address, amount) {
+  return await callRPC('createstakingvault', [address, amount]);
+}
+
+export async function getVaultBalance(address) {
+  return await callRPC('getstakingvaultbalance', [address]);
+}
+
+export async function getVaultInfo(address) {
+  return await callRPC('getstakingvaultinfo', [address]);
+}
+
+export async function listVaults() {
+  return await callRPC('liststakingvaults');
+}
+
+export async function getTransaction(txid) {
+  return await callRPC('gettransaction', [txid]);
+}
+
+export async function sendToAddress(address, amount) {
+  return await callRPC('sendtoaddress', [address, amount]);
+}
+
 
 
 
