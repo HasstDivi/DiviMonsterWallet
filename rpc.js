e09d095a80bd25b036667b156c0927fc284fb8f1
HasstDivi <jrmal2021@gmail.com>
2025-07-12 21:23:36 +0200

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,23 +1,21 @@
+// rpc.js
 const RPC_USER = "DivinoMonstruo";
 const RPC_PASSWORD = "divipachuli123";
 const RPC_PORT = 51473;
-const RPC_HOST = "http://138.68.94.212"; // IP de tu VPS
+const RPC_HOST = "138.68.94.212";
 
 async function rpcCall(method, params = []) {
-  const body = {
-    jsonrpc: "1.0",
-    id: "curltest",
-    method,
-    params,
-  };
-
-  const response = await fetch(`${RPC_HOST}:${RPC_PORT}/`, {
+  const response = await fetch(`http://${RPC_HOST}:${RPC_PORT}/`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Basic " + btoa(`${RPC_USER}:${RPC_PASSWORD}`),
     },
-    body: JSON.stringify(body),
+    body: JSON.stringify({
+      jsonrpc: "1.0",
+      id: "DiviMonsterWallet",
+      method,
+      params,
+    }),
   });
 
   const data = await response.json();
