fc85bfa8f5852d885bdfbc17e13b01d3d5ceb24c
HasstDivi <jrmal2021@gmail.com>
2025-07-15 20:42:35 +0200

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -135,5 +135,21 @@ async function verRecompensas() {
     alert("❌ Error al consultar recompensas.");
     console.error(e);
   }
+  async function probarConexion() {
+  try {
+    const respuesta = await fetch('http://138.68.94.212:3000/getblockcount', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({})
+    });
+
+    const datos = await respuesta.json();
+    alert('✅ Conectado. Altura de bloque: ' + datos.result);
+  } catch (error) {
+    alert('❌ Error de conexión con el backend.');
+    console.error('Error al conectar:', error);
+  }
 }
+
+
 
